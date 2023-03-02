109621f5d360cc0328d844924423393b560df515
Chris Vargas <124189207+Chrisawgey@users.noreply.github.com>
2023-03-01 22:34:42 -0500

    Add files via upload

---
+++ app/head.js
@@ -0,0 +1,8 @@
+const Head = () => (
+  <>
+    <title>InterludeScope</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+  </>
+);
+
+export default Head;
---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/jox.png',
+    title: 'Project One',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/mask.png',
+    title: 'Project two',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/teddychill.png',
+    title: 'Project three',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/coffeebot.png',
+    title: 'Project four',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/asrto.png',
+    title: 'Project five',
+  },
+];
+
+export const startingFeatures = [
+  'Things that I am Mostly Intrested in',
+  'Enter the Blog to know my hobbies and Likings.',
+  'Example Text',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    subtitle:
+        'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: '7 tips to easily master the madness of the Metaverse',
+    subtitle:
+        'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'With one platform you can explore the whole world virtually',
+    subtitle:
+        'Quam quisque id diam vel quam elementum. Viverra nam libero justo laoreet sit amet cursus sit. Mauris in aliquam sem',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
